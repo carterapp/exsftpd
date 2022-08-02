f77bb4178ec84b64fff5908befdbeffc718145fc
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2022-08-02 18:52:37 +0200

    update licence text so SPDX recognises it

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Exsftpd.MixProject do
     [
       # This option is only needed when you don't want to use the OTP application name
       name: "exsftpd",
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/Codenaut/exsftpd"}
     ]
   end
